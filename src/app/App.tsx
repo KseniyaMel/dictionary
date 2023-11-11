2753665f0f34bbc990f928251b2eb76144f5bd72
Мелихова Ксения <kmelikhova@data-platform.ru>
2023-11-11 12:48:51 +0300

    fixed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Header } from '../components/Header/Header';
 import Glossary from '../pages/glossary';
 import MindMap from '../pages/mindMap';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Header />}>
-          <Route path="/glossary" element={<Glossary />} />
-          <Route path="/semantic-graph" element={<MindMap />} />
+          <Route path="glossary" element={<Glossary />} />
+          <Route path="semantic-graph" element={<MindMap />} />
           <Route
             path="*"
-            element={<Navigate to="/glossary" />}
+            element={<Navigate to="glossary" />}
           />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
